0e2c3d0f9df69b2a8788f5b816b0301b9bff2b58
0x0abc123 <v3natoris@gmail.com>
2023-07-07 08:11:29 +1000

    Commit 1688681489

--- leakypy.py
+++ leakypy.py
@@ -20,3 +20,87 @@ aws_akid = 'AKIAIOSFODNN7N0EFXMP'
 aws_sk = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYBNA2dBkDTiv'
 
 print(f'{aws_akid}')
+
+import boto3
+# ruleid: boto-client-ip
+client = boto3.client(host="192.168.1.200")
+# ok: boto-client-ip
+client = boto3.client(host="dev.internal.example.com")
+foo = {
+    'id': 'A_3',
+    'test': '408yF3hfausdhvfa963g^F*@^D',
+    'length': 7,
+    'password':'S2j7^gY+SfPJg)yuzy_D]?~}9_'
+    }
+def something(a,b,c):
+    hardcoded = '72b59f9e-5bb2-4501-a83e-55fe675ce4fa'
+    print(f'x: {hardcoded}, and {a},{b},{c}')
+
+xyz = 'test string with nothing interesting in it'
+something(1,3,'72b59f9e-5bb2-4501-a83e-55fe675ce4fa')
+something(1,3,'api_key_72b59f9e-5bb2-4501-a83e-55fe675ce4fa')
+testfunc('ac',['8937428','token=312406cd-fff7-439d-ab2e-831b9855ee17'])
+print(f'blah-de-blah-2893')
+entropytest = 'aa_bc_98nv'
+uninteresting = "secret_value=na$c%u5gc737rggwGgfHaXgsCcbhbs"
+key='BASICAUTH_BASE64STRING=pZMTAwWTEwVTVDU09TRVMFNPU1FBVEFXU0BRQQ=='
+if paesswd == '%H,m+S2j7^gY+SfPJg)y' and something('a',None,None):
+    something(1,3,4)
+elif passwd == '%H,m+S2j7^gY+SfPJg)y' and something('b',None,None):
+    something(1,3,5)
+
+if passwd == '%H,m+S2j7^gY+SfPJg)y' or something('a',None,None):
+    something(1,3,4)
+if passwd == '%H,m+S2j7^gY+SfPJg)y':
+    something(1,3,4)
+if passwd != '%H,m+S2j7^gY+SfPJg)y':
+    something(1,3,4)
+nkvxn = ['sdf',None,'abg','api_key=TCVILG0rUzJqN15nWStTZlBKZyl5dXp5X0RdP359OV8K']
+if (entropytest != 'xxx' or 'f2740g*:Rc7%0+udsg=f6aGEgHg9' in nkvxn):
+    pass
+if (passwd == 'f2740g*:Rc7%0+udsg=f6aGEgHg9'):
+    pass
+if request.headers['x-auth-token'] == '72b59f9e-5bb2-4501-a83e-55fe675ce4fa':
+    pass
+nothinginteresting['foo_stuff'] = 'password=15nWStTZlBKZyl5dXp5X' 
+response.headers['x-auth'] = 'f2740g*:Rc7%0+udsg=f6aGEgHg9'
+test.foo(options={'id':'OI1740SGE52WFGG','passw':'TCVILG0rUzJqN15nWStTZlBKZyl5dXp5X0RdP359OV8K'},False)
+data = {
+    'something': 'xwjs656e2gpw5w2oiem4k0hfb',
+    'env':[
+        "TEST=1234",
+        "DB_PWORD=I-LG0rU,zJqN15nWStT"
+    ]
+}
+data2 = {
+    'something': 'xwjs656e2gpw5w2oiem4k0hfb',
+    'test':[
+        "TEST_ID=1234",
+        "DB_PASSWORD=I-LG0rU,zJqN15nWStT",
+        "URL=https://www.testsite.com/route/db"
+    ]
+}
+data3 = {
+    'obj':{
+        'abc':'/path/to/file_hfa73hbUKXcuYasdfjh.ext',
+        'values':"password=a73hbUKXcuYa!"
+    },
+    'list_of_things':[
+        "ABC129387",
+        "DEF93426"
+    ]
+}
+data4 = ('test','api-key=wjs65GdS56e2gpw5w2oiem4k0hfb')
+obj.do_something(["test","password=an7693wtfudgis6%^$Q"])
+obj.do_something2(opts=["test","password=an7693wtfudgis6%^$Q"])
+user_dict = {'username': 'password', 'address': 'zipcode'}
+creds = {'password':None,'user':''}
+password_value = ''
+password_value = 'low_entropy'
+password_value = 'Cookies2023!'
+creds = {
+    'password': '',
+    'user': ''
+    }
+requests.get('http://api.foobar.com/api/v2/users?id=73897')
+requests.get('https://api.foobar.com/api/v2/users?authtoken=0rUzJqN15nWStTZlBKZyl5dXp5X0RdP359OV8')
